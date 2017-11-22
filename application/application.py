34bf43a5f28fc62a3410f5b6d7167f455bfd93dd
donaldjones09 <donald.jones600@gmail.com>
2017-11-22 11:59:29 -0500

    progress

--- application/application.py
+++ application/application.py
@@ -2,23 +2,25 @@ from flask import *
 from flask_sqlalchemy import SQLAlchemy
 from models import *
 
-ath_ID = 0
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/athleteindex', methods = ["GET"])
+@app.route('/athleteindex', methods = ["GET", "POST"])
 def athleteindex():
     if request.method == "GET":
         rows = Athlete.query.all()
         return render_template('athleteindex.html', rows = rows)
     else:
-        ath_ID = request.form.get['athlete']
-        return redirect(url_for('athletebio'))
+        ath_ID = int(request.form.get("athlete"))
+        print("ath_ID = "+str(ath_ID))
+        row = Athlete.query.filter_by(ath_ID = ath_ID)
+        return render_template('athletebio.html', row = row)
 
 
-@app.route('/athletebio', methods = ["GET"])
-def athletebio():
-    athlete = Athlete.query.filter_by(ath_ID = ath_ID)
-    return render_template('athletebio.html')
+#@app.route('/athletebio', methods = ["GET"])
+#def athletebio():
+#    print ("ath_ID now = " + str(ath_ID))
+#    row = Athlete.query.filter_by(ath_ID = ath_ID)
+#    return render_template('athletebio.html', row = row)
